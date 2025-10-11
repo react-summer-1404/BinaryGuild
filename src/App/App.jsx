5e6980d7ca9b7200b8886e87ab41aa3a84d8b994
Zahra <zahra2114.ss@gmail.com>
2025-10-11 18:04:34 +0330

    update

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     
-    <main dir={i18n.language === "fa" ? "rtl" : "ltr"}>
+    <main dir={i18n.language === "fa"}>
       <RouterProvider router={Router}/>
     </main>
   )
