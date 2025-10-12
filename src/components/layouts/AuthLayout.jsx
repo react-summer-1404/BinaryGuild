d0fab2dafe2f1cbe929de7a4c2df8fe544861acf
Zahra <zahra2114.ss@gmail.com>
2025-10-12 15:56:41 +0330

    update

--- src/components/layouts/AuthLayout.jsx
+++ src/components/layouts/AuthLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 import Auth from "../common/footer/authenticationCommon/Auth";
 const AuthLayout = () => {
   return (
-    <div className="bg-[#FCFCFC] font-sans min-h-screen p-10 m-auto flex gap-20 justify-center w-screen">
+    <div className="bg-[#FCFCFC] font-sans min-h-screen p-10 m-auto  flex gap-20 justify-center w-screen">
       <div className="border-3 rounded-4xl p-10 w-1/2 bg-gray-200">
         <Auth />
       </div>
