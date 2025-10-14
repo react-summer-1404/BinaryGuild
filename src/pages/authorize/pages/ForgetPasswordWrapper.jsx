1c275dcd8c94f487aaf970e0c8ca5353ff21d3f2
Zahra <zahra2114.ss@gmail.com>
2025-10-14 20:49:36 +0330

    update forgetPass

--- src/pages/authorize/pages/ForgetPasswordWrapper.jsx
+++ src/pages/authorize/pages/ForgetPasswordWrapper.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { useState } from 'react';
-import Stepper from '../../Stepper';
-import GoToGmail from '../steps/GoToGmail';
-import FirstStepPassword from '../steps/FirstStepPassword';
+import React from "react";
+import { useState } from "react";
+import Stepper from "../../Stepper";
+import GoToGmail from "../steps/GoToGmail";
+import FirstStepPassword from "../steps/FirstStepPassword";
 const ForgetPasswordWrapper = () => {
-    const [step, setStep] = useState(1);
-    console.log("step", step)
+  const [step, setStep] = useState(1);
+  console.log("step", step);
   return (
-    <div className='p-5 flex flex-col text-right w-[648px] mr-[30px] '>
-            <div className="flex justify-start">
-        <Stepper text={"واردکردن ایمیل"} active={step === 1}/>
-        <Stepper text={"تایید کد ارسال شده"} active={step === 2}/>
+    <div className="p-5 flex flex-col text-right w-[648px] mr-[30px] ">
+      <div className="flex justify-start">
+        <Stepper text={"واردکردن ایمیل"} active={step === 1} />
+        <Stepper text={"تایید کد ارسال شده"} active={step === 2} />
       </div>
-      {step === 1 && <GoToGmail onNext = {()=>setStep(2)}/> }
-      {step === 2 && <FirstStepPassword  onPrevious={()=>setStep(1)}/> }
+      {step === 1 && <GoToGmail onNext={() => setStep(2)} />}
+      {step === 2 && <FirstStepPassword onPrevious={() => setStep(1)} />}
     </div>
-  )
-}
+  );
+};
 
-export default ForgetPasswordWrapper
+export default ForgetPasswordWrapper;
--- src/pages/authorize/steps/FirstStepPassword.jsx
+++ src/pages/authorize/steps/FirstStepPassword.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import React from "react";
+import AuthButton from "../../../components/common/button/AuthButton";
+import { useState } from "react";
+const FirstStepPassword = ({ onPrevious }) => {
+  const [getEmail, setGetEmail] = useState("");
+  const [getPassword, setGetPassword] = useState("");
+  const [emailError, setEmailError] = useState(null);
+  const [passError, setPassError] = useState();
 
-const FirstStepPassword = () => {
+  const handleEmail = (e) => {
+    const value = e.target.value;
+    setGetEmail(value);
+  };
+
+  const handlePass = (e) => {
+    const value = e.target.value;
+    setGetPassword(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (getEmail.trim() === "") {
+      setEmailError("لطفا رمز عبور خود را وارد کنید ");
+    } else {
+      setEmailError(null);
+    }
+
+    if (getPassword.trim() === "") {
+      setPassError("لطفا تکرار رمز عبور خود را وارد کنید ");
+    } else {
+      setPassError(null);
+    }
+
+    if (getEmail.trim() !== "" && getPassword.trim() !== "") {
+      console.log("فرم درسته");
+    }
+  };
+
   return (
-    <div>FirstStepPassword</div>
-  )
-}
+    <div className="w-4/5">
+      <h2 className="text-[28px] font font-[700] text-black mt-[75px]">
+        رمزعبور جدید!{" "}
+      </h2>
+      <p className="text-[#707070] font-[500] mt-[12px] text-[16px]">
+        {" "}
+        رمزعبور جدید خود را وارد کنید
+      </p>
 
-export default FirstStepPassword
+      <div>
+        <form
+          onSubmit={handleSubmit}
+          action=""
+          className="flex flex-col mt-[48px]"
+        >
+          <label
+            className="text-[#2F2F2F] font-[600] text-[16px]"
+            htmlFor="newPass"
+          >
+            رمز عبور جدید{" "}
+          </label>
+          <input
+            className="mt-[8px] w-[398px] h-[48px] border-1 p-[16px] rounded-[24px] border-[#DCDCDC] text-[#707070] font-[500] text-[14px]"
+            type="password"
+            id="newPass"
+            value={getEmail}
+            onChange={handleEmail}
+            placeholder="رمز عبور خود را وارد کنید"
+          />
+          <p className="mt-[4px] font-bold text-[12px] text-[red]">
+            {emailError}
+          </p>
+          <label
+            className="text-[#2F2F2F] mt-4 font-[600] text-[16px]"
+            htmlFor="newpass"
+          >
+            تکرار رمز عبور جدید{" "}
+          </label>
+          <input
+            className="mt-[8px] w-[398px] h-[48px] border-1 p-[16px] rounded-[24px] border-[#DCDCDC] text-[#707070] font-[500] text-[14px]"
+            type="password"
+            id="newpass"
+            value={getPassword}
+            onChange={handlePass}
+            placeholder="تکرار رمز عبور خود را وارد کنید"
+          />
+          <p className="mt-[4px] font-bold text-[12px] text-[red]">
+            {passError}
+          </p>
+          <AuthButton text={" تایید رمز عبور"} type="submit" />
+        </form>
+      </div>
+
+      <div className=" w-[397px] flex flex-col items-center justify-center ">
+        <div
+          onClick={onPrevious}
+          className="cursor-pointer mt-[32px] flex items-center justify-center border-[1px] border-[#DCDCDC] rounded-[34px] w-[141px] h-[40px]"
+        >
+          <p className="text-[#3772FF]">{"بازگشت"}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FirstStepPassword;
