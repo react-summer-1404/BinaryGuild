21dc9da14ff3866ecc7d2dcf475d0fa5d249a8ee
Zahra <zahra2114.ss@gmail.com>
2025-10-16 14:54:35 +0330

    call api register

--- src/pages/authorize/steps/GetUserInfo.jsx
+++ src/pages/authorize/steps/GetUserInfo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import AuthButton from "../../../components/common/button/AuthButton";
 import { useState } from "react";
-const GetUserInfo = ({ onPrevious }) => {
+import axios from "axios";
+const GetUserInfo = ({ onPrevious, confirm,phoneNumber }) => {
   const [getEmail, setGetEmail] = useState("");
   const [getPassword, setGetPassword] = useState("");
   const [emailError, setEmailError] = useState(null);
   const [passError, setPassError] = useState();
-  
 
   const handleEmail = (e) => {
     const value = e.target.value;
@@ -18,31 +18,56 @@ const GetUserInfo = ({ onPrevious }) => {
     setGetPassword(value);
   };
 
-  const handleSubmit=(e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if(getEmail.trim() === ""){
+    if (getEmail.trim() === "" ) {
       setEmailError("ایمیل نمی تواند خالی باشد");
-    }else{setEmailError(null)}
-
-    if(getPassword.trim() === ""){
-      setPassError("لطفا رمز عبور خود را وارد کنید ");
-    }else{setPassError(null)} 
-    
-    if(getEmail.trim() !== "" && getPassword.trim() !== ""){
-      console.log("فرم درسته")
+    } else {
+      setEmailError(null);
     }
-  }
 
+    if (getPassword.trim() === "") {
+      setPassError("لطفا رمز عبور خود را وارد کنید ");
+    } else {
+      setPassError(null);
+    }
 
+    if (getEmail.trim() !== "" && getPassword.trim() !== "") {
+      console.log("فرم درسته");
+    }
+
+    try {
+      const response = await axios.post(
+        "https://sepehracademy.liara.run/Sign/Register",
+        {
+          password: getPassword,
+          gmail: getEmail,
+          phoneNumber: phoneNumber,
+        }
+      );
+      console.log(response.data, "response");
+      confirm();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="w-4/5">
-
-      <h2 className="text-[28px] font font-[700] text-black mt-[75px]">وارد کردن اطلاعات شخصی</h2>
-      <p className="text-[#707070] font-[500] mt-[12px] text-[16px]">   لطفا اطلاعات اولیه خواسته شده را وارد کنید </p>
+      <h2 className="text-[28px] font font-[700] text-black mt-[75px]">
+        وارد کردن اطلاعات شخصی
+      </h2>
+      <p className="text-[#707070] font-[500] mt-[12px] text-[16px]">
+        {" "}
+        لطفا اطلاعات اولیه خواسته شده را وارد کنید{" "}
+      </p>
 
       <div>
-        <form onSubmit={handleSubmit} action="" className="flex flex-col mt-[48px]">
+        <form
+          onSubmit={handleSubmit}
+          action=""
+          className="flex flex-col mt-[48px]"
+        >
           <label
             className="text-[#2F2F2F] font-[600] text-[16px]"
             htmlFor="email"
@@ -57,7 +82,9 @@ const GetUserInfo = ({ onPrevious }) => {
             onChange={handleEmail}
             placeholder="ایمیل خود را وارد کنید"
           />
-          <p className="mt-[4px] font-bold text-[12px] text-[red]">{emailError}</p>
+          <p className="mt-[4px] font-bold text-[12px] text-[red]">
+            {emailError}
+          </p>
           <label
             className="text-[#2F2F2F] font-[600] text-[16px]"
             htmlFor="password"
@@ -72,20 +99,21 @@ const GetUserInfo = ({ onPrevious }) => {
             onChange={handlePass}
             placeholder="رمزعبور خود را وارد کنید"
           />
-          <p className="mt-[4px] font-bold text-[12px] text-[red]">{passError}</p>
-      <AuthButton text={" تایید"} type="submit"/>
-
+          <p className="mt-[4px] font-bold text-[12px] text-[red]">
+            {passError}
+          </p>
+          <AuthButton text={" تایید"} type="submit" />
         </form>
       </div>
 
       <div className=" w-[397px] flex flex-col items-center justify-center ">
         <div
           onClick={onPrevious}
-          className="cursor-pointer mt-[32px] flex items-center justify-center border-[1px] border-[#DCDCDC] rounded-[34px] w-[141px] h-[40px]">
+          className="cursor-pointer mt-[32px] flex items-center justify-center border-[1px] border-[#DCDCDC] rounded-[34px] w-[141px] h-[40px]"
+        >
           <p className="text-[#3772FF]">{"بازگشت"}</p>
         </div>
       </div>
-
     </div>
   );
 };
