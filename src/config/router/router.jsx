c59aad01252a2b420bc3fc1b996d38f80667b191
melika <melikamosavi1717@gmail.com>
2025-11-06 18:18:24 +0330

    refactor: change import to lazy import for prformance loading pags

--- src/config/router/router.jsx
+++ src/config/router/router.jsx
@@ -1,21 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
-import AuthLayout from "../../components/layouts/AuthLayout";
-import LandingLayout from "../../components/layouts/LandingLayout";
-import Courses from "../../pages/Courses";
-import ForgetPasswordWrapper from "../../screen/authentication/pages/ForgetPasswordWrapper";
-import LoginWrapper from "../../screen/authentication/pages/LoginWrapper";
-import RegisterWrapper from "../../screen/authentication/pages/RegisterWrapper";
-import LandingWrapper from "../../screen/landing-wrapper/LandingWrapper";
-import StudentPanelLayout from "../../components/layouts/StudentPanelLayout";
-import CourseDetailWrapper from "../../../src/screen/course-detail/CourseDetailWrapper"
-import SetNewPassword from "../../pages/authorize/steps/SetNewPassword";
-import BlogsDetailWrapper from "../../screen/blogs-detail/BlogsDetailWrapper";
-import MyCourse from "../../pages/student-panel/my-courses/MyCourse";
-import Dashboard from "../../pages/student-panel/dashboard/Dashboard";
-import MyReserve from "../../pages/student-panel/my-reserve-course/MyReserve";
-import UserProfile from "../../pages/student-panel/profile/UserProfile";
-import CourseFav from "../../pages/student-panel/course-fav/CourseFav";
-import BlogFav from "../../pages/student-panel/blog-fav/BlogFav";
+import { lazy } from "react";
+
+// Layouts
+const AuthLayout = lazy(() => import("../../components/layouts/AuthLayout"));
+const LandingLayout = lazy(() => import('../../components/layouts/LandingLayout'));
+const StudentPanelLayout = lazy(() => import("../../components/layouts/StudentPanelLayout"));
+
+// Pages
+const ForgetPasswordWrapper = lazy(() => import("../../screen/authentication/ForgetPasswordWrapper"));
+const LoginWrapper = lazy(() => import("../../screen/authentication/LoginWrapper"));
+const RegisterWrapper = lazy(() => import("../../screen/authentication/RegisterWrapper"));
+const LandingWrapper = lazy(() => import("../../screen/landing-wrapper/LandingWrapper"));
+const CourseDetailWrapper = lazy(() => import("../../screen/course-detail/CourseDetailWrapper"));
+const SetNewPassword = lazy(() => import("../../pages/authorize/steps/SetNewPassword"));
+const BlogsDetailWrapper = lazy(() => import("../../screen/blogs-detail/BlogsDetailWrapper"));
+
+// Student Panel
+const MyCourse = lazy(() => import("../../pages/student-panel/my-courses/MyCourse"));
+const Dashboard = lazy(() => import("../../pages/student-panel/dashboard/Dashboard"));
+const MyReserve = lazy(() => import("../../pages/student-panel/my-reserve-course/MyReserve"));
+const UserProfile = lazy(() => import("../../pages/student-panel/profile/UserProfile"));
+const CourseFav = lazy(() => import("../../pages/student-panel/course-fav/CourseFav"));
+const BlogFav = lazy(() => import("../../pages/student-panel/blog-fav/BlogFav")); 
 
 const Router = createBrowserRouter([
   {
@@ -43,7 +49,7 @@ const Router = createBrowserRouter([
   },
   {
     path: "/panel",
-    element: <StudentPanelLayout />,
+    element: <StudentPanelLayout />, 
     children: [
       { path: "my-panel", element: <MyCourse/> },
       { path: "", element: <Dashboard/> },
